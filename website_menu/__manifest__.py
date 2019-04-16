c12c4f36605d3717b6d0bb8fb8a4472544a49417
blouk <nicolas@blouk.com>
2019-04-16 16:50:41 +0200

    [ADD] Manisfest

---
+++ website_menu/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Website Menu',
+
+    'category': 'Hidden',
+    'version': '1.0',
+    'author': 'Odoo S.A.(nwi@odoo.com)',
+    'depends': ['website'],
+    'data': [
+    
+
+    ],
+}
